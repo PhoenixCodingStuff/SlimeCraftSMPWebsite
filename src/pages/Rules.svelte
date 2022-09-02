77a8e06a6882720101bdc788a125041a2e6eef11
PhoenixCodingStuff <kunalgk.2503@gmail.com>
2022-09-02 18:05:24 +0530

    ✨ Added rules info

--- src/pages/Rules.svelte
+++ src/pages/Rules.svelte
@@ -3,5 +3,58 @@
 </script>
 
 <main>
-  Rules page content here...
+    <html>
+        <head>
+            <style>
+                h1 {text-align: center;}
+                h2 {text-align: center;}
+                p {text-align: center;}
+            </style>
+        </head>
+        <body>
+            <h1>SlimeCraftSMP Rules Disclaimer</h1>
+                <p>
+                    <br>SlimeCraftSMP is a public server but we reserve all rights to control who plays here and who does not!</br>
+                    <br>These rules apply to everyone and are to be followed regardless of you viewing or understanding them.</br>
+                    <br><i>Not knowing rules is not a reason to break them!</i></br>
+                    <br>Staff have the right to handle situations and punishments at their own discretion.</br>
+                    <br>These rules are subject to change at any time.</br>
+                    <br>If you have questions about the rules, please open a ticket on Discord</br>
+                    <br>Do not support people who break rules or you will be banned as well!</br>
+                </p>
+            <h2>SlimeCraftSMP Gameplay Rules</h2>
+                <p>
+                    <br><u><b>NSFW-</b></u> 18+ skins , cape , username , building etc will get you banned.</br>
+                    <br><u><b>DDoS-</b></u> DDoS threats will get you instantly and permanently banned.</br>
+                    <br><u><b>Lag Machines-</b></u> Making lag machines will get you instantly and permanently banned.</br>
+                    <br><u><b>Cheating-</b></u> Using Hacked Clients , X-Ray , Mods , Ghost Clients , Macros (Auto Mining, Fishing, Etc) to automatically gather resources will get you banned. For disallowed / allowed mods please create a ticket on discord.</br>
+                    <br><u><b>Exploiting-</b></u> Do not exploit a bug / dupe to your advantage. Not reporting it to staff and sharing it with other players will get you banned.</br>
+                    <br><u><b>Griefing-</b></u> Modifying another player’s land irrespective of the fact that it is claimed or unclaimed without permission will get you banned.</br>
+                    <br><u><b>Killing/Trapping-</b></u> Killing / Trapping other players without their consent and taking their stuff will get you banned.</br>
+                    <br><u><b>Terrain/Structure Griefing-</b></u> Do not leave floating trees , do not place random blocks , please keep the terrain smooth. Breaking chests from ANY structure is not allowed!</br>
+                    <br><u><b>Alts-</b></u> Each person on an IP address can use 2 accounts. If you have an alt account you <b>MUST</b> notify staff about it. Using alts to evade punishments will get you permanently banned.</br>
+                </p>
+            <h2>SlimeCraftSMP Chat Rules [Applies on all chat platforms]</h2>
+                <p>
+                    <br><u><b>General Behavious-</b></u> Do not be toxic / curse excessively and be kind and respectful towards others.</br>
+                    <br><u><b>Advertising-</b></u> Advertising a server name/IP or Discord is bannable. Also applies to private messages and Discord DMs.</br>
+                    <br><u><b>Automod Evasion-</b></u> Evading the swear filter by using symbols etc will get you permanently muted.</br>
+                    <br><u><b>Doxing-</b></u> Do not share someone's personal info like email , phone number etc without the person's consent or you will be banned.</br>
+                    <br><u><b>Scam-</b></u> Any type of malicious link , IP grabber etc will get you banned.</br>
+                    <br><u><b>Spamming-</b></u> Spamming chat with messages will get you muted. Also includes command spamming.</br>
+                    <br><u><b>Harassment/Discrimination-</b></u> Do not be rude to people of certain races, genders etc. Includes slurs which will get you muted permanently.</br>
+                    <br><u><b>Impersonation-</b></u> Do not pretend to be a player / staff you are not and mislead others or you will be banned.</br>
+                    <br><u><b>Arguing With Staff-</b></u> Do not argue with a staff member on a punishment or rule. Do not be disruptive when a staff member is doing their job.</br>
+                    <br><u><b>Drama-</b></u> Do not pick fights with people on the server and do not purposely trigger another person will get you muted.</br>
+                    <br><u><b>Begging-</b></u> Do not beg for OP , staff roles , creative or to unban others!</br>
+                </p>
+            <h2>SlimeCraftSMP Discord Rules</h2>
+                <p>
+                    <br><u><b>NSFW-</b></u> No inappropriate imagery [Profile picture included].</br>
+                    <br><u><b>VC Disruption-</b></u> Do not play ear rape in VC using bots or mic.</br>
+                    <br><u><b>Channel-</b></u> Use channels for the respective topic</br>
+                    <br><u><b>TOS-</b></u> All Discord ToS and Guidelines apply, including 13+ only allowed!</br>
+                </p>
+        </body>
+    </html>
 </main>
