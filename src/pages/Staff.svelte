051fb4b719a3c8212589025fd8f1005014dbea8d
PhoenixCodingStuff <kunalgk.2503@gmail.com>
2022-09-02 18:30:50 +0530

    ✨ Added staff info

--- src/pages/Staff.svelte
+++ src/pages/Staff.svelte
@@ -3,5 +3,45 @@
 </script>
 
 <main>
-  Staff page content here...
+    <html>
+        <head>
+            <style>
+                h1 {text-align: center;}
+                p {text-align: center;}
+            </style>
+        </head>
+        <body>
+            <h1 style="color:red;"> Owners</h1>
+                <p style="color:red;">
+                    <br>Phoenix_Playz_YT / PhoenixPlayz#1880</br>
+                    <br>Evilgamer6969 / Evilgamer6969#9083</br>
+                    <br>saiarjun546 / saiarjun546#8915</br>
+                </p>
+            <h1 style="color:orange;"> Admins</h1>
+                <p style="color:orange;">
+                    <br>Fl__uffy / Fl__uffy#5899</br>
+                </p>
+            <h1 style="color:orange;"> Developers</h1>
+                <p style="color:orange;">
+                    <br>poopjoek / Forkbomb#8587</br>
+                </p>
+            <h1 style="color:yellow;"> Moderators</h1>
+                <p style="color:yellow;">
+                    <br>Astegon / Eratix#8506</br>
+                    <br>MinecraftIsaac7 / Isaac_the_dest#8133</br>
+                </p>
+            <h1 style="color:lime;"> Helpers</h1>
+                <p style="color:lime;">
+                    <br>None. You can apply by creating a ticket on discord!</br>
+                </p>
+            <h1 style="color:blue;"> Media</h1>
+                <p style="color:blue;">
+                    <br>None. You can apply by creating a ticket on discord!</br>
+                </p>
+            <h1 style="color:indigo;"> Artists</h1>
+                <p style="color:indigo;">
+                    <br>NegativeIonEn / NegativeIonEntity#8497</br>
+                </p>
+        </body>
+    </html>
 </main>
